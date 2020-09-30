93af84c8ef9fad120fc2ba595f1aa592cbe53ceb
Ary Vidot <aryvid@gmail.com>
2020-09-30 12:13:07 +0100

    contact form addded, fawesome cdn updated

--- js/Form.js
+++ js/Form.js
@@ -1,19 +1,19 @@
 //  Listen for form submit
 
-document.getElementById("submit").addEventListener("submit", submitForm);
+// document.getElementById("submit").addEventListener("submit", submitForm);
 
-function submitForm(e) {
-  e.preventDefault();
-  console.log(123);
+// function submitForm(e) {
+//   e.preventDefault();
+//   console.log(123);
 
-  // get values
-  const name = getInputVal("name");
-  const email = getInputVal("email");
-  const subject = getInputVal("subject");
-  const message = getInputVal("message");
-}
+//   // get values
+//   const name = getInputVal("name");
+//   const email = getInputVal("email");
+//   const subject = getInputVal("subject");
+//   const message = getInputVal("message");
+// }
 
-// function to get form values
-function getInputVal(id) {
-  return document.getElementById(id).nodeValue;
-}
+// // function to get form values
+// function getInputVal(id) {
+//   return document.getElementById(id).nodeValue;
+// }
